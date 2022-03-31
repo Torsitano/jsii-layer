8467de8c1ac812a274022f02aa5a5a02e300fcbd
Shawn Torsitano <shawn.torsitano@gmail.com>
2022-03-31 18:56:10 -0400

    removed projen

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+var _a;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Greeter = void 0;
+const JSII_RTTI_SYMBOL_1 = Symbol.for("jsii.rtti");
+class Greeter {
+    constructor(props) {
+        this.greetee = props.greetee;
+    }
+    greet() {
+        return `Hello, ${this.greetee}!`;
+    }
+}
+exports.Greeter = Greeter;
+_a = JSII_RTTI_SYMBOL_1;
+Greeter[_a] = { fqn: "jsii-layer.Greeter", version: "1.0.0" };
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7OztBQUlFLE1BQWEsT0FBTztJQUdsQixZQUFtQixLQUFtQjtRQUNwQyxJQUFJLENBQUMsT0FBTyxHQUFHLEtBQUssQ0FBQyxPQUFPLENBQUM7SUFDL0IsQ0FBQztJQUVNLEtBQUs7UUFDVixPQUFPLFVBQVUsSUFBSSxDQUFDLE9BQU8sR0FBRyxDQUFBO0lBQ2xDLENBQUM7O0FBVEgsMEJBVUMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgaW50ZXJmYWNlIEdyZWV0ZXJQcm9wcyB7XHJcbiAgICByZWFkb25seSBncmVldGVlOiBzdHJpbmc7XHJcbiAgfVxyXG4gIFxyXG4gIGV4cG9ydCBjbGFzcyBHcmVldGVyIHtcclxuICAgIHByaXZhdGUgcmVhZG9ubHkgZ3JlZXRlZTogc3RyaW5nO1xyXG4gIFxyXG4gICAgcHVibGljIGNvbnN0cnVjdG9yKHByb3BzOiBHcmVldGVyUHJvcHMpIHtcclxuICAgICAgdGhpcy5ncmVldGVlID0gcHJvcHMuZ3JlZXRlZTtcclxuICAgIH1cclxuICBcclxuICAgIHB1YmxpYyBncmVldCgpOiBzdHJpbmcge1xyXG4gICAgICByZXR1cm4gYEhlbGxvLCAke3RoaXMuZ3JlZXRlZX0hYFxyXG4gICAgfVxyXG4gIH0iXX0=
