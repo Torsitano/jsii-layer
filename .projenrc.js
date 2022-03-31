0a93aa4f8711f42bd21bb31870b6de4e3f7ffd85
Shawn Torsitano <shawn.torsitano@gmail.com>
2022-03-31 18:14:48 -0400

    adding jsii

--- .projenrc.js
+++ .projenrc.js
@@ -16,7 +16,10 @@ const project = new cdk.JsiiProject({
   },
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
+  devDeps: [
+    jsii,
+    jsii-pacmak
+  ],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
 project.setScript('format', 'prettier -w "src/**/*.ts"');
